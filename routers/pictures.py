ed158c941225d420a76ef4678669da3bffc7a60f
Vinicius Roque <hownatios@gmail.com>
2023-03-29 17:52:05 -0300

    feat: add zip file upload and exceptions to function

--- routers/pictures.py
+++ routers/pictures.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, UploadFile, File
-from typing import Annotated
+from typing import List
+
+from fastapi import APIRouter, UploadFile
 
 from logic.pictures_services import Pictures
+from utils.exceptions.exception import InvalidPathOrFile
+from utils.middlewares.files_service import extract_files
 from utils.middlewares.google_credentials_provider import (
     get_cloud_storage_client,
 )
@@ -14,16 +17,14 @@ pic_router = APIRouter(prefix="/pictures", tags=["pictures"])
 client = get_cloud_storage_client()
 
 
-@pic_router.post("/files")
-async def create_file(file: Annotated[bytes, File()]):
-    return {"file_size": len(file)}
+@pic_router.post("/upload/{file_path:path}")
+async def upload_file(file_path: str, file: List[UploadFile]):
+    is_folder = file_or_folder_validator(file_path)
+    if is_folder:
+        files_to_upload = await extract_files(file)
+        return await Pictures.upload_files(files_to_upload, file_path)
 
-
-@pic_router.post("/upload")
-async def upload_file(file: UploadFile):
-    print("Entrei!")
-    print(file.filename)
-    return {"filename": "file.filename"}
+    raise InvalidPathOrFile("There were problems with the order")
 
 
 @pic_router.get("/{file_path:path}")

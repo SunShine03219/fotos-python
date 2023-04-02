f0fddb85e9ad3dd34c7dd9d97a6e2b44b8251c9c
loliveirawebdev <loliveirawebdev@gmail.com>
2023-04-02 11:08:12 -0300

    feat: "ok" route

--- routers/home.py
+++ routers/home.py
@@ -7,3 +7,8 @@ home_router = APIRouter(tags=["home"])
 @home_router.get("/favicon.ico")
 async def favicon():
     return Response(status_code=204)
+
+
+@home_router.get("/")
+async def favicon():
+    return "Ok"
